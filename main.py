ab538e19959f418010fa997b31e9b97e2f6e7fa3
ShawonAshraf <shawon13@live.com>
2023-02-23 23:05:32 +0100

    collator test

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import argparse
+
+
+from data.dataset import Conll06Dataset
+from data.collator import data_collator_fn
+from torch.utils.data import DataLoader
+
+
+if __name__ == "__main__":
+    p = "/home/shawon/Projects/parser-data/english/train/wsj_train.conll06"
+
+    trainset = Conll06Dataset(p)
+    train_loader = DataLoader(trainset, batch_size=16, drop_last=True, num_workers=8, collate_fn=data_collator_fn)
+    
+    for td in train_loader:
+        print(type(td["heads"]))
+        break
+    
