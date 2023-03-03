889fb6b6c4c14e82471fafdbfb5de4fa55e12d10
ShawonAshraf <shawon13@live.com>
2023-03-03 03:18:38 +0100

    clean up

--- model/parser.py
+++ model/parser.py
@@ -1,83 +1,12 @@
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
-from transformers import AutoModel
 import torch.nn.functional as F
 import torch.optim as optim
 from einops import rearrange
 from .mlp import MLP
 
+
 class ParserTransformer(pl.LightningModule):
-    def __init__(self, lr: float,
-                 parser_heads: int,
-                 parser_rels: int,
-                 ignore_idx: int) -> None:
+    def __init__(self) -> None:
         super().__init__()
-
-        self.lr = lr
-        self.save_hyperparameters()
-
-        self.parser_heads = parser_heads
-        self.parser_rels = parser_rels
-        self.ignore_idx = ignore_idx
-
-        self.distil_bert = AutoModel.from_pretrained("microsoft/xtremedistil-l6-h384-uncased")
-
-        self.head_labeler = MLP(384, 150, self.parser_heads)
-        # self.head_labeler = nn.Linear(384, self.parser_heads)
-        self.rel_labeler = MLP(384, 150, self.parser_rels)
-        # self.rel_labeler = nn.Linear(384, self.parser_rels)
-        # self.dropout = nn.Dropout(0.1)
-
-        self.loss_fn_head = nn.CrossEntropyLoss()
-        self.loss_fn_rel = nn.CrossEntropyLoss()
-
-    def forward(self, input_ids: torch.Tensor, attention_mask: torch.Tensor):
-        out = self.distil_bert(input_ids=input_ids, attention_mask=attention_mask)
-        out = out.pooler_output
-
-        head_logits = self.head_labeler(out)
-        head_logits = F.relu(head_logits)
-
-        rel_logits = self.rel_labeler(out)
-        rel_logits = F.relu(rel_logits)
-
-        return head_logits, rel_logits
-
-    def configure_optimizers(self):
-        return optim.Adam(lr=self.lr, params=self.parameters())
-
-    def training_step(self, batch, batch_idx):
-        input_ids = batch["input_ids"]
-        attention_mask = batch["attention_mask"]
-        heads = batch["heads"]
-        rels = batch["rels"]
-
-        heads = rearrange(heads, "bs 1 seq -> bs seq")
-        rels = rearrange(rels, "bs 1 seq -> bs seq")
-
-        head_logits, rel_logits = self(input_ids, attention_mask)
-
-        loss = self.loss_fn_head(head_logits, heads) + self.loss_fn_rel(rel_logits, rels)
-
-        return {
-            "loss": loss,
-            "log": {
-                "training_loss": loss
-            }
-        }
-
-    def validation_step(self, batch, batch_idx):
-        input_ids = batch["input_ids"]
-        attention_mask = batch["attention_mask"]
-        heads = batch["heads"]
-        rels = batch["rels"]
-
-        heads = rearrange(heads, "bs 1 seq -> bs seq")
-        rels = rearrange(rels, "bs 1 seq -> bs seq")
-
-        head_logits, rel_logits = self(input_ids, attention_mask)
-
-        loss = self.loss_fn_head(head_logits, heads) + self.loss_fn_rel(rel_logits, rels)
-
-        self.log("validation_loss", loss, prog_bar=True)
