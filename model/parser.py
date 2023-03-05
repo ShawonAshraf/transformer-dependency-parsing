ca20681ec2d4e9a90809a8850ac9443e782b3ff2
ShawonAshraf <shawon13@live.com>
2023-03-05 06:22:06 +0100

    back to hf again

--- model/parser.py
+++ model/parser.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Dict
 
 import pytorch_lightning as pl
 import torch
@@ -6,9 +6,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from einops import rearrange
+from transformers import AutoModel, AutoTokenizer
+from transformers.tokenization_utils_base import BatchEncoding
 
 from .mlp import MLP
-from .positional_encoder import PositionalEncoder
 
 """
     with default transformer properties
@@ -18,38 +19,25 @@ from .positional_encoder import PositionalEncoder
 
 class ParserTransformer(pl.LightningModule):
     def __init__(self,
-                 vocab_size: int,
-                 max_len: int,
                  n_parser_heads: int,
                  n_rels: int,
                  ignore_index: int,
-                 mlp_hidden: int = 300,
-                 n_encoder_layers: int = 6,
-                 n_heads: int = 8,
-                 d_model: int = 512,
+                 encoder_pretrained_name: str,
+                 hidden: int = 300,
                  lr: float = 1e-3) -> None:
         super().__init__()
 
-        self.vocab_size = vocab_size
-        self.mlp_hidden = mlp_hidden
-        self.d_model = d_model
-        self.n_heads = n_heads
-        self.n_encoders = n_encoder_layers
-        self.max_len = max_len
+        self.hidden = hidden
         self.lr = lr
+        self.encoder_pretrained_name = encoder_pretrained_name
 
         self.save_hyperparameters()
 
         self.n_parser_heads = n_parser_heads
         self.n_rels = n_rels
 
-        # embedding layer
-        self.embedding = nn.Embedding(
-            num_embeddings=self.vocab_size, embedding_dim=self.d_model)
-
-        # positional encoder
-        self.positional_encoder = PositionalEncoder(
-            d_model=self.d_model, max_len=self.max_len)
+        # encoder gives word embeddings
+        self.encoder = AutoModel.from_pretrained(self.encoder_pretrained_name)
 
         # transformers for alignment
         self.sentences_head_transformer = None
@@ -58,53 +46,43 @@ class ParserTransformer(pl.LightningModule):
         self.sentence_rel_transformer = None
         self.relu2 = nn.ReLU()
 
-        # mlp for classification
-        self.head_classifier = MLP(
-            self.d_model, self.mlp_hidden, self.n_parser_heads)
-        self.rel_classifier = MLP(self.d_model, self.mlp_hidden, self.n_rels)
+        # decoders as classifier
+        # mlp
+
+        decoder_in = 384
+
+        self.head_decoder = MLP(
+            decoder_in, self.hidden, self.n_parser_heads)
+        self.rel_decoder = MLP(decoder_in, self.hidden, self.n_rels)
 
         # loss function
         self.criterion = nn.NLLLoss(ignore_index=ignore_index)
 
-    def forward(self, sentence: torch.Tensor,
-                heads: torch.Tensor,
-                rels: torch.Tensor):
+    def forward(self, input_ids: torch.Tensor,
+                attention_mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        encoded = self.encoder(
+            input_ids=input_ids,
+            attention_mask=attention_mask)
+        pooler = encoded.pooler_output
 
-        s = self.embedding(sentence) * torch.sqrt(torch.tensor(self.d_model))
-        h = self.embedding(heads) * torch.sqrt(torch.tensor(self.d_model))
-        r = self.embedding(rels) * torch.sqrt(torch.tensor(self.d_model))
+        out1 = self.head_decoder(pooler)
+        out1 = F.log_softmax(out1, dim=-1)
 
-        s = rearrange(s, "bs seq embed -> seq bs embed")
-        h = rearrange(h, "bs seq embed -> seq bs embed")
-        r = rearrange(r, "bs seq embed -> seq bs embed")
+        out2 = self.rel_decoder(pooler)
+        out2 = F.log_softmax(out2, dim=-1)
 
-        s = self.positional_encoder(s)
-        h = self.positional_encoder(h)
-        r = self.positional_encoder(r)
-
-        out1 = self.sentences_head_transformer(s, h)
-        out1 = self.relu1(out1)
-        out1 = self.head_classifier(out1)
-
-        out2 = self.sentence_rel_transformer(s, r)
-        out2 = self.relu2(out2)
-        out2 = self.rel_classifier(out2)
-
-        return F.log_softmax(out1, dim=-1), F.log_softmax(out2, dim=-1)
+        return out1, out2
 
     def configure_optimizers(self):
         return optim.AdamW(params=self.parameters())
 
     def training_step(self, batch, batch_idx):
-        sentence = batch["sentence"]
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
         heads = batch["heads"]
         rels = batch["rels"]
 
-        out1, out2 = self(sentence, heads, rels)
-
-        # rearrange for loss
-        out1 = rearrange(out1, "seq bs probas -> bs probas seq")
-        out2 = rearrange(out2, "seq bs probas -> bs probas seq")
+        out1, out2 = self(input_ids, attention_mask)
 
         loss = self.criterion(out1, heads) + self.criterion(out2, rels)
 
@@ -116,15 +94,15 @@ class ParserTransformer(pl.LightningModule):
         }
 
     def validation_step(self, batch, batch_idx):
-        sentence = batch["sentence"]
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
         heads = batch["heads"]
         rels = batch["rels"]
 
-        out1, out2 = self(sentence, heads, rels)
+        out1, out2 = self(input_ids, attention_mask)
 
-        # rearrange for loss
-        out1 = rearrange(out1, "seq bs probas -> bs probas seq")
-        out2 = rearrange(out2, "seq bs probas -> bs probas seq")
+        print(out1.size())
+        print(out2.size())
 
         loss = self.criterion(out1, heads) + self.criterion(out2, rels)
 
